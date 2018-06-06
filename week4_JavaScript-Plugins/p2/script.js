568198d0507372b935cd41c05047b550858f093e
Kenny Lee <kennyklee@gmail.com>
2018-06-05 21:54:25 -0700

    week4 p2 init

---
+++ week4_JavaScript-Plugins/p2/script.js
@@ -0,0 +1,18 @@
+// For this project, you’ll use the free IP-API to get a user’s location. You can use that location to get the weather data itself from MetaWeather, which also has an API for weather icons.
+// http://ip-api.com/docs/api:json
+// https://www.metaweather.com/api/
+
+// For this plugin, you should setup some user-configurable options. Some potential options include things like:
+
+// * Whether to show temperatures in Fahrenheit of Celsius.
+// * What message to show (ex. It's currently {temperature} and {conditions} in {location}).
+// * Whether or not to show an icon for the weather conditions.
+
+// Some considerations
+// This will require multiple API calls to several different endpoints. What’s the best way to structure your code? How do you avoid insane, nested XHR calls? What happens if there’s an error somewhere along the way?
+
+/**
+ * QUESTIONS / COMMENT
+ */
+ // The nested XHR call seems to be a jump.
+ // What is JSONP? Do I need to be using it?
