16829e6d09b3c2a2709a455fab559747d407b7ff
Kenny Lee <kennyklee@gmail.com>
2018-06-16 23:16:36 -0700

    w4-p1 refactor

--- week4_JavaScript-Plugins/p1/script.js
+++ week4_JavaScript-Plugins/p1/script.js
@@ -1,8 +1,17 @@
+/**
+ * IIFE: Controls tabs
+ * @return APIs:
+ *              tabs.init()
+ *              tabs.selectTab()
+ *
+ */
+
 var tabs = (function () {
+
     /**
      * Variables
      */
-    var tabs = document.querySelector('.tabs');
+
     var allTabPanes = document.querySelectorAll('.tab-pane'); // All of the tab contents [NodeList]
     var showTab = 0; // Default tab. '0' is the first tab.
     var publicApis = {};
@@ -10,23 +19,50 @@ var tabs = (function () {
     /**
      * Methods
      */
+
+    // Initializer
+    publicApis.init = function () {
+        document.addEventListener('click', publicApis.selectTab);
+        publicApis.selectTab(showTab); // Show first tab (0);
+    }
+
+    /**
+     * Show contents of matching tab
+     * @param   {tabName}   matching tab id to be displayed
+     */
     var showTabContent = function (tabName) {
         Array.from(allTabPanes).forEach( function (tabPane) {
-            tabPane.style.display = 'none'; // Hide all
+            tabPane.style.display = 'none'; // Hides all first
             if (tabPane.id === tabName) tabPane.style.display = 'block'; // Show if match
         });
     }
 
-    publicApis.chooseTab = function (event) { // Assign to global
+    /*
+     * Input options:
+     * click,
+     * tabs.selectTab([int]),
+     * tabs.selectTab([name of tab])
+    */
+
+    /**
+     * Handle click events
+     * @param  {Event} event The click event
+     * Examples:
+     *     click,
+     *     tabs.selectTab([id of tab]),
+     *     tabs.selectTab([index of tab]),
+     */
+
+    publicApis.selectTab = function (event) { // Assign to global
         if (event === 'undefined') return;
 
         if (event.target) { // If mouse click
             if(!event.target.matches('[data-tab]')) return;
-            showTab = event.target.hash.substr(1); // Name of clicked tab
-        } else if (typeof event === 'string') {
-            showTab = event; // Name of clicked tab
-        } else if (typeof event === 'number') {
-            showTab = allTabPanes[event].id; // Name of clicked tab
+            showTab = event.target.hash.substr(1);
+        } else if (typeof event === 'string') { // If 'string', id of tab
+            showTab = event;
+        } else if (typeof event === 'number') { // If 'nmmber', index of tab
+            showTab = allTabPanes[event].id;
         } else {
             console.error('Click on tab, enter tab #id string, or tab number. Try again.');
         }
@@ -34,17 +70,8 @@ var tabs = (function () {
         showTabContent(showTab); // Show the tab.
     };
 
-    /**
-     * Init & Listeners
-     */
-    document.addEventListener('click', publicApis.chooseTab);
-    publicApis.chooseTab(showTab); // Show first tab;
+    return publicApis;
 
-    return publicApis; // where does publicApis go?
-
-    /**
-     * Questions - How come this doesn't work?
-     */
-    // var clickTab2 = function () {console.log("whatevez")};
-    // return clickTab2;
 })()
+
+tabs.init()
